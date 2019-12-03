53d9c89b8573e2ada0da8a1ff5030cebfe0c19e0
Beza <bezaworkoffice@gmail.com>
2019-12-03 00:08:10 +0000

    removing bootstrap from index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import "./index.css";
 import "./theme.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import "bootstrap/dist/css/bootstrap.min.css";
 import WebFont from "webfontloader";
 
 WebFont.load({
